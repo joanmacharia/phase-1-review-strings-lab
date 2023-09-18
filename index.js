a6462e338de274017e2b85a15fb7193c7ceec6a7
Joan Macharia <joan.macharia@students.moringa>
2023-09-18 16:17:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+
+
+const currentUser = 'Joan';
+
+// const welcomeMessage = 'Welcome to Flatbook,'
+
+const welcomeMessage = "Welcome to Flatbook, " + '' + currentUser + '!'
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`
